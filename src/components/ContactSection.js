8d030351289203dce727573d354e7af1cc4134c2
Kaleel Aadil <cb010188@students.apiit.lk>
2025-06-05 20:33:32 +0530

    added fevicon

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -23,36 +23,36 @@ function ContactSection() {
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
           {/* Phone Section */}
           <div className="bg-white p-8 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <div className="text-red-500 text-4xl mb-4">
+            <div className="text-black text-4xl mb-4">
               <FaPhoneAlt />
             </div>
             <h3 className="text-xl font-semibold mb-2">Phone</h3>
-            <p className="text-lg text-gray-700">+82 10 3932 2357</p>
+            <p className="text-lg text-black">+82 10 3932 2357</p>
           </div>
 
           {/* Fax Section */}
           <div className="bg-white p-8 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <div className="text-gray-600 text-4xl mb-4">
+            <div className="text-black text-4xl mb-4">
               <FaFax />
             </div>
             <h3 className="text-xl font-semibold mb-2">Fax</h3>
-            <p className="text-lg text-gray-700">+82 70 4131 8944</p>
+            <p className="text-lg text-black">+82 70 4131 8944</p>
           </div>
 
           {/* Email Section */}
           <div className="bg-white p-8 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <div className="text-blue-500 text-4xl mb-4">
+            <div className="text-black text-4xl mb-4">
               <FaEnvelope />
             </div>
             <h3 className="text-xl font-semibold mb-2">Email</h3>
             <p className="text-lg text-gray-700">
-              <a href="mailto:kfsb23@naver.com" className="text-blue-500">kfsb23@naver.com</a>
+              <a href="mailto:kfsb23@naver.com" className="text-black">kfsb23@naver.com</a>
             </p>
           </div>
 
           {/* Address Section */}
           <div className="bg-white p-8 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <div className="text-green-500 text-4xl mb-4">
+            <div className="text-black text-4xl mb-4">
               <FaMapMarkerAlt />
             </div>
             <h3 className="text-xl font-semibold mb-2">Address</h3>
